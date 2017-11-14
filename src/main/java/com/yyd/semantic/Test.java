6461ecd081aaef0d32763b526d8d58e3750d2e39
celery <liuqing@yydrobo.com>
2017-11-14 14:21:52 +0800

    commit code

--- src/main/java/com/yyd/semantic/Test.java
+++ src/main/java/com/yyd/semantic/Test.java
@@ -1,12 +1,18 @@
 package com.yyd.semantic;
 
+import java.util.List;
+
+import com.yyd.semantic.nlp.NLPFactory;
 import com.yyd.semantic.nlp.SegSceneParser;
+import com.yyd.semantic.nlp.WordTerm;
 
 public class Test {
 
 	public static void main(String[] args) {
-		String service = SegSceneParser.parse("我想听李白的静夜思");
+		String service = SegSceneParser.parse("我想听刘德华的歌");
 		System.out.println(service);
+		List<WordTerm> result = NLPFactory.segment("我想听张学友的饿狼传说");
+		System.out.println(result);
 	}
 
 }
