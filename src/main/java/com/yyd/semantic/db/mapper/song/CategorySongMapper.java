49c0b142ffa6e23ffc72680ab0b51c830c1bbd7f
celery <liuqing@yydrobo.com>
2017-11-21 20:56:49 +0800

    sql add rang

--- src/main/java/com/yyd/semantic/db/mapper/song/CategorySongMapper.java
+++ src/main/java/com/yyd/semantic/db/mapper/song/CategorySongMapper.java
@@ -7,6 +7,6 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface CategorySongMapper {
-	@Select("SELECT resource_id FROM music.music_category_resource WHERE category_id=#{categoryId}")
+	@Select("SELECT resource_id FROM music.music_category_resource WHERE category_id=#{categoryId} ORDER BY RAND() LIMIT 10")
 	public List<Integer> getSongIdsByCategoryId(Integer categoryId);
 }
